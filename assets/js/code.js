13c0fa1af451d019b0be8a04b568c7a21eb8b40e
unxok <anderluke707@gmail.com>
2022-07-08 23:42:58 -0700

    forgot to delete some old firebase code

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,25 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDPPVQcBjfyMzz-m4aF_UGB27G1k820G9s",
-  authDomain: "unxok-test.firebaseapp.com",
-  projectId: "unxok-test",
-  storageBucket: "unxok-test.appspot.com",
-  messagingSenderId: "491874157424",
-  appId: "1:491874157424:web:2497db40fe5976eb35c5b5",
-  measurementId: "G-D18QN5Y224",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 function greeting() {
   let a = document.getElementById("hello");
   let x = a.value;
